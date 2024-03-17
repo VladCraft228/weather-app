8a3dad59cabe21b67ecb90128253dc5dba768bee
VladCraft228 <vladikgavrilyuk02@gmail.com>
2024-03-17 03:15:45 +0200

    improved usability of the application

--- src/components/WeatherData.vue
+++ src/components/WeatherData.vue
@@ -1,62 +1,83 @@
 <template>
-  <div>
-    <h3>What's the weather like in the city?</h3>
+  <div class="q-ma-md">
+    <!-- Перевірка, чи вже отримано погоду -->
+    <div v-if="!weather">
+      <h3 v-if="showTitle">What's the weather like in the city?</h3>
 
-    <!-- Поле пошуку за назвою міста, які відображається, коли не використовуються координати -->
-    <div v-if="!useCoordinates">
-      <q-input
-        outlined
-        v-model="city"
-        placeholder="Enter the name of the city"
-        :dense="dense"
-        @keyup.enter="getWeather"
-        label-position="right"
-      >
-        <!-- Кнопка для пошуку погоди за назвою міста -->
-        <template v-slot:append>
-          <q-icon style="cursor: pointer" @click="getWeather" name="search">
-          </q-icon>
-        </template>
-      </q-input>
+      <!-- Поле пошуку за назвою міста, які відображається, коли не використовуються координати -->
+      <div v-if="!useCoordinates">
+        <q-input
+          outlined
+          class="q-mb-md"
+          v-model="city"
+          placeholder="Enter the name of the city"
+          :dense="dense"
+          @keyup.enter="getWeather"
+          label-position="right"
+          v-if="!weather"
+        >
+          <!-- Кнопка для пошуку погоди за назвою міста -->
+          <template v-slot:append>
+            <q-icon style="cursor: pointer" @click="getWeather" name="search">
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+
+      <!-- Поля для введення координат, які відображаються, коли використовуються координати -->
+      <div class="coord-input" v-else>
+        <q-input
+          type="number"
+          outlined
+          v-model="latitudeLeft"
+          class="q-mb-md"
+          label="Latitude (integer part)"
+          :dense="dense"
+        />
+        <q-input
+          type="number"
+          outlined
+          v-model="latitudeRight"
+          class="q-mb-md"
+          label="Latitude (decimal part)"
+          :dense="dense"
+        />
+        <q-input
+          type="number"
+          outlined
+          v-model="longitudeLeft"
+          class="q-mb-md"
+          label="Longitude (integer part)"
+          :dense="dense"
+        />
+        <q-input
+          type="number"
+          outlined
+          v-model="longitudeRight"
+          class="q-mb-md"
+          label="Longitude (decimal part)"
+          :dense="dense"
+        />
+
+        <!-- Кнопка для пошуку погоди за координатами міста -->
+        <q-btn
+          label="Search"
+          color="primary"
+          padding="15px"
+          @click="getWeatherByCoordinates"
+          v-if="!weather"
+        />
+      </div>
+
+      <!-- Перемикач для вибору режиму пошуку за координатами або назвою міста -->
+      <q-toggle
+        v-model="useCoordinates"
+        label="Search by coordinates"
+        v-if="!weather"
+      />
     </div>
 
-    <!-- Поля для введення координат, які відображаються, коли використовуються координати -->
-    <div class="coord-input" v-else>
-      <q-input
-        type="number"
-        outlined
-        v-model="latitudeLeft"
-        label="Latitude (integer part)"
-        :dense="dense"
-      />
-      <q-input
-        type="number"
-        outlined
-        v-model="latitudeRight"
-        label="Latitude (decimal part)"
-        :dense="dense"
-      />
-      <q-input
-        type="number"
-        outlined
-        v-model="longitudeLeft"
-        label="Longitude (integer part)"
-        :dense="dense"
-      />
-      <q-input
-        type="number"
-        outlined
-        v-model="longitudeRight"
-        label="Longitude (decimal part)"
-        :dense="dense"
-      />
-
-      <!-- Кнопка для пошуку погоди за координатами міста -->
-      <q-btn label="Search" color="primary" @click="getWeatherByCoordinates" />
-    </div>
-
-    <!-- Перемикач для вибору режиму пошуку за координатами або назвою міста -->
-    <q-toggle v-model="useCoordinates" label="Search by coordinates" />
+    <!-- Показувати погоду тільки якщо вона отримана -->
     <div v-if="weather">
       <!-- Код для відображення погодних даних, коли вони доступні -->
       <h2>
@@ -67,6 +88,18 @@
       <p>Feeling like: {{ weather.main.feels_like }}°C</p>
       <p>Description: {{ weather.weather[0].description }}</p>
       <p>Wind speed: {{ weather.wind.speed }}</p>
+      <q-icon
+        style="
+          cursor: pointer;
+          color: white;
+          background-color: #1976d2;
+          padding: 20px;
+          border-radius: 5px;
+          float: right;
+        "
+        name="keyboard_backspace"
+        @click="resetSearch"
+      ></q-icon>
     </div>
   </div>
 </template>
@@ -84,20 +117,22 @@ export default {
       weather: null,
       dense: true,
       useCoordinates: false,
+      showTitle: true, // Додайте змінну для відображення/приховання заголовка
     };
   },
   methods: {
     // Метод для отримання погодних даних за містом
     getWeather() {
+      this.previousWeather = this.weather;
       const API_KEY = "ea8f3c3618b948e676e5d468c0ca7bfd";
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${API_KEY}&units=metric`;
-
       // Перевірка на пусту строку
       if (!this.city.trim()) {
         this.$q.notify({
           type: "negative",
           message: "Please enter a city name",
         });
+        this.showTitle = true; // показуємо заголовок
         return; // Повертаємо, щоб не виконувати запит на сервер
       }
       fetch(url)
@@ -108,14 +143,18 @@ export default {
               type: "negative",
               message: "This city does not exist, try something else",
             });
+            this.weather = this.previousWeather;
+            this.showTitle = true; // показуємо заголовок
           } else {
             this.weather = data;
+            this.showTitle = false; // приховуємо заголовок
           }
         });
     },
 
     // Метод для отримання погоди за координатами
     getWeatherByCoordinates() {
+      this.previousWeather = this.weather;
       // Перевірка на пусті поля для координат
       if (
         this.latitudeLeft === null ||
@@ -138,7 +177,6 @@ export default {
         });
         return;
       }
-
       const latitude = Number(`${this.latitudeLeft}.${this.latitudeRight}`);
       const longitude = Number(`${this.longitudeLeft}.${this.longitudeRight}`);
       const API_KEY = "ea8f3c3618b948e676e5d468c0ca7bfd";
@@ -153,10 +191,25 @@ export default {
               type: "negative",
               message: "This city does not exist, try something else",
             });
+            this.weather = this.previousWeather;
+            this.showTitle = true; // показуємо заголовок
           } else {
             this.weather = data;
+            this.showTitle = false; // приховуємо заголовок
           }
         });
+    },
+
+    resetSearch() {
+      this.weather = null;
+      this.previousWeather = null;
+      this.showSearch = true;
+      this.showTitle = true;
+      this.city = "";
+      this.latitudeLeft = 0;
+      this.latitudeRight = 0;
+      this.longitudeLeft = 0;
+      this.longitudeRight = 0;
     },
 
     // Метод для відображення відповідного значка погоди на основі опису погоди
